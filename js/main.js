9dd14b2f9742c31b3f5bc80f40060b1190d04125
Iyinoluwa <iyinoyelz69@gmail.com>
2024-02-19 19:45:25 -0700

    fixes links on home page

--- js/main.js
+++ js/main.js
@@ -146,16 +146,16 @@ ScrollReveal({
     delay : 100
 
  });
-ScrollReveal().reveal('.home .info h2 , .section-title-01,.section-title-02', {delay:500,origin:'left' });
-ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay:600,origin:'right' });
-ScrollReveal().reveal('.home .info .btn ', {delay: 700 , origin:'bottom'});
-ScrollReveal().reveal('.media-icons i , .contact-left li', {delay: 500 , origin:'left', interval:200});
-ScrollReveal().reveal('.home-img , .about-img', {delay: 500 , origin:'bottom'});
-ScrollReveal().reveal('.about .description, .contact-right', {delay: 500 , origin:'right', interval:200});
-ScrollReveal().reveal('.about .professional-list li', {delay: 500 , origin:'right', interval:200});
-ScrollReveal().reveal('.skills-description , .services-description, .contact-card, .client-slider, .contact-left h2', {delay: 700 , origin:'left'});
-ScrollReveal().reveal('.experience-card , .service-card ,.education, .portfolio .img-card', {delay: 800 , origin:'bottom', interval:200});
-ScrollReveal().reveal('footer .group', {delay: 500 , origin:'top', interval:200});
+ScrollReveal().reveal('.home .info h2 , .section-title-01,.section-title-02', {delay:300,origin:'left' });
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay:300,origin:'right' });
+ScrollReveal().reveal('.home .info .btn ', {delay: 300 , origin:'bottom'});
+ScrollReveal().reveal('.media-icons i , .contact-left li', {delay: 200 , origin:'left', interval:200});
+ScrollReveal().reveal('.home-img , .about-img', {delay: 200 , origin:'bottom'});
+ScrollReveal().reveal('.about .description, .contact-right', {delay: 200 , origin:'right', interval:200});
+ScrollReveal().reveal('.about .professional-list li', {delay: 200 , origin:'right', interval:200});
+ScrollReveal().reveal('.skills-description , .services-description, .contact-card, .client-slider, .contact-left h2', {delay: 200 , origin:'left'});
+ScrollReveal().reveal('.experience-card , .service-card ,.education, .portfolio .img-card', {delay: 200 , origin:'bottom', interval:200});
+ScrollReveal().reveal('footer .group', {delay: 200 , origin:'top', interval:200});
 
 
 
